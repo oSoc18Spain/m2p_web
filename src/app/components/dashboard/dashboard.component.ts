62625c641b1d151b1a804460826a8c2ea0a94cfb
Máximo García Martínez <maximogarciamtnez@gmail.com>
2018-07-12 20:16:23 +0200

    now cards display automatically

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,23 +2,94 @@ import { Component, OnInit } from '@angular/core';
 
 import {MatTableModule} from '@angular/material/table';
 
+export interface Event {
+  title: string;
+  description: string;
+  status: string;
+  time: Date;
+  id: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {name: 'Helium', weight: 4.0026, symbol: 'He'}
-  ]
-  a = [{p:123},{p:456}]
+  
+  event_list: Event[] = [
+    {
+      title:"Evento 1",
+      description: "Es una descripcioin",
+      status:"pending",
+      time: new Date(),
+      id: 22
+    },
+    {
+      title:"Evento 2",
+      description: "Es una descripcion",
+      status:"pending",
+      time: new Date(),
+      id: 23
+    },
+    {
+      title:"Evento 2",
+      description: "Es una descripcion",
+      status:"pending",
+      time: new Date(),
+      id: 23
+    }
+  ];
+  worker1: Event[] = [
+    {
+      title:"Evento 5",
+      description: "Es una descripcioin",
+      status:"in_progress",
+      time: new Date(),
+      id: 29
+    }
+  ];
+  done_list: Event[] = [
+    {
+      title:"Evento 3",
+      description: "Es una descripcioin",
+      status:"done",
+      time: new Date(),
+      id: 25
+    },
+    {
+      title:"Evento 4",
+      description: "Es una descripcioin",
+      status:"done",
+      time: new Date(),
+      id: 27
+    }
+  ];
+
+  datatable = {
+    status:200,
+    columns:[
+      {
+        name:"no_assigment",
+        event_list:this.event_list
+      },
+      {
+        name:"worker1",
+        event_list:this.worker1
+      },
+      {
+        name:"done",
+        event_list:this.done_list
+      }
+    ]
+  }
+  
   constructor() {
   }
-  //eventos: tarea1, tarea2
-
   ngOnInit() {
+    //Calculate the number of columns and it changes the CSS
+    const ncolumns = this.datatable.columns.length;
+    document.getElementById('table').style.gridTemplateColumns = `repeat(${ncolumns}, 1fr)`;
   }
 
 }
