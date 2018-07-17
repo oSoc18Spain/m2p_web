7ebc8001a5b06480a168e86cae830c955db2649a
max <maximogarciamtnez@gmail.com>
2018-07-17 20:34:54 +0200

    added fake info

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { DashboardService } from './dashboard.service'
+import { DashboardService } from './dashboard.service';
 import { Datatable } from '../../models';
 import { environment } from '../../../environments/environment';
-
 
 @Component({
   selector: 'app-dashboard',
@@ -10,43 +9,40 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  SERVER: string =  environment.server
-  PORT: number = environment.port
+  SERVER: string = environment.server;
+  PORT: number = environment.port;
   datatable: Datatable = {
     status: 0,
     columns: []
   };
-  
+
   constructor(private dashboard: DashboardService) {}
 
   ngOnInit() {
     this.getDashboardSubscription();
   }
 
-  getColumns = () => {
-    return this.datatable.columns;
-  }
-
   changeColumns = () => {
     //Calculate the number of columns and it changes the CSS
-    const ncolumns = this.datatable.columns.length;    
+    const ncolumns = this.datatable.columns.length;
     document
       .getElementById('table')
       .style.setProperty('grid-template-columns', `repeat(${ncolumns}, 1fr)`);
-      }
+  };
 
-    getDashboardSubscription = () => {
-      let datatable = this.dashboard.getDashboardJSON()
-      .subscribe((data: Datatable) => {
-        if(data.status !== 200){
-          throw new Error(`Couldn't connect to the API Server at ${this.SERVER}:${this.PORT}`)
-        }else{
-          data.columns.map(e => {
-            e.time = new Date(e.timeInSeconds);
-          })
-          this.datatable = {...data};
-          this.changeColumns()
-        }
-      })
-    }
+  getDashboardSubscription = () => {
+    this.dashboard.getDashboardJSON().subscribe((data: Datatable) => {
+      if (data.status !== 200) {
+        throw new Error(
+          `Couldn't connect to the API Server at ${this.SERVER}:${this.PORT}`
+        );
+      } else {
+        data.columns.map(e => {
+          e.time = new Date(e.timeInSeconds);
+        });
+        this.datatable = { ...data };
+        this.changeColumns();
+      }
+    });
+  };
 }
