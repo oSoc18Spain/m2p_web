f2cf9b01a59696f6a660e8ac18c16ee456bb4f82
Máximo García Martínez <maximogarciamtnez@gmail.com>
2018-07-18 12:39:24 +0200

    added line-info

---
+++ src/app/components/home/line-info/line-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-info',
+  templateUrl: './line-info.component.html',
+  styleUrls: ['./line-info.component.css']
+})
+export class LineInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
